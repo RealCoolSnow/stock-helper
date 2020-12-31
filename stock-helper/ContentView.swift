2ccff25a783bdc343801e37056f9363a02d581cf
CoolSnow <670696652@qq.com>
2020-12-31 16:20:00 +0800

    update

---
+++ stock-helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stock-helper
+//
+//  Created by CoolSnow on 2020/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stock-helper/stock_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  stock_helperApp.swift
+//  stock-helper
+//
+//  Created by CoolSnow on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct stock_helperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
