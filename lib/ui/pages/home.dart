28f379cbdd1930fc46a97a4cc24678c0dc6184a9
CoolSnow <670696652@qq.com>
2021-01-04 17:35:41 +0800

    update

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -3,20 +3,42 @@ import 'package:stock_helper/locale/i18n.dart';
 import 'package:stock_helper/ui/pages/home/setting.dart';
 import 'package:stock_helper/ui/pages/home/stock_list.dart';
 
+const int TAB_LENGTH = 2;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  int _tabSelected = 0;
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: TAB_LENGTH, vsync: this);
+    _tabController.addListener(() {
+      if (_tabController.index.toDouble() == _tabController.animation.value) {
+        setState(() {
+          _tabSelected = _tabController.index;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: _buildAppBar(),
-        body: _buildBody(),
-      ),
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+      floatingActionButton: _buildActionButton(), // Thi
     );
   }
 
@@ -30,6 +52,7 @@ class _HomePageState extends State<HomePage> {
 
   Widget _buildTabBar() {
     return TabBar(
+      controller: _tabController,
       indicatorColor: Colors.white,
       tabs: <Widget>[
         Tab(icon: Icon(Icons.home)),
@@ -39,6 +62,21 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildBody() {
-    return TabBarView(children: [StockList(), Setting()]);
+    return TabBarView(
+        controller: _tabController, children: [StockList(), Setting()]);
+  }
+
+  Widget _buildActionButton() {
+    return _tabSelected == 0
+        ? FloatingActionButton(
+            onPressed: _addStock,
+            tooltip: I18n.of(context).text("add_stock"),
+            child: Icon(Icons.add),
+          )
+        : null;
+  }
+
+  void _addStock() {
+    print('_addStock');
   }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,7 +7,10 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    self.setContentSize(NSSize(width: 400, height: 640))
+    let window:NSWindow! = self.contentView?.window
+    window.styleMask.remove(.resizable)
+    
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
