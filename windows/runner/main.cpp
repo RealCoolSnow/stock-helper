9756c70884a60e056e284a5e3592aacb167398b6
CoolSnow <coolsnow2020@gmail.com>
2021-02-01 14:18:53 +0800

    update windows

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -29,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(400, 640);
   if (!window.CreateAndShow(L"stock_helper", origin, size)) {
     return EXIT_FAILURE;
   }
